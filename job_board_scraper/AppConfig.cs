1fc959658c353107bc9892fd21dfe616f02e9ca7
GSU <gsu1234@mail.ru>
2025-08-28 20:58:15 +0300

    Небольшие изменения

--- job_board_scraper/AppConfig.cs
+++ job_board_scraper/AppConfig.cs
@@ -3,10 +3,10 @@
 public static class AppConfig
 {
     public static readonly char[] Chars = "abcdefghijklmnopqrstuvwxyz0123456789-_".ToCharArray();
-    public static readonly string BaseUrl = "http://career.habr.com/";
-    public static readonly int MinLength = 4;
-    public static readonly int MaxLength = 4;
-    public static readonly int MaxConcurrentRequests = 5;
-    public static readonly int MaxRetries = 200;
-    public static readonly string ConnectionString = "Server=localhost:5432;User Id=postgres;Password=admin;Database=jobs;";
+    public const string BaseUrl = "http://career.habr.com/";
+    public const int MinLength = 4;
+    public const int MaxLength = 4;
+    public const int MaxConcurrentRequests = 5;
+    public const int MaxRetries = 200;
+    public const string ConnectionString = "Server=localhost:5432;User Id=postgres;Password=admin;Database=jobs;";
 }
