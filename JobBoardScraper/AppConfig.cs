c182f0d25756b1f8e4dba580b28f4979effc9b94
GSU <gsu1234@mail.ru>
2025-10-29 21:08:24 +0300

    Рефактор конфигурации

--- JobBoardScraper/AppConfig.cs
+++ JobBoardScraper/AppConfig.cs
@@ -1,12 +1,36 @@
-﻿namespace JobBoardScraper;
+﻿using System.Configuration;
 
+namespace JobBoardScraper;
+
+/// <summary>
+/// Конфигурация приложения, читается из App.config
+/// </summary>
 public static class AppConfig
 {
-    public static readonly char[] Chars = "abcdefghijklmnopqrstuvwxyz0123456789-_".ToCharArray();
-    public const string BaseUrl = "http://career.habr.com/";
-    public const int MinLength = 5;
-    public const int MaxLength = 5;
-    public const int MaxConcurrentRequests = 5;
-    public const int MaxRetries = 200;
-    public const string ConnectionString = "Server=localhost:5432;User Id=postgres;Password=admin;Database=jobs;";
+    // Настройки для BruteForceUsernameScraper
+    public static char[] Chars => (ConfigurationManager.AppSettings["BruteForce:Chars"] ?? "abcdefghijklmnopqrstuvwxyz0123456789-_").ToCharArray();
+    
+    public static string BaseUrl => ConfigurationManager.AppSettings["BruteForce:BaseUrl"] ?? "http://career.habr.com/";
+    
+    public static int MinLength => int.TryParse(ConfigurationManager.AppSettings["BruteForce:MinLength"], out var value) ? value : 5;
+    
+    public static int MaxLength => int.TryParse(ConfigurationManager.AppSettings["BruteForce:MaxLength"], out var value) ? value : 5;
+    
+    public static int MaxConcurrentRequests => int.TryParse(ConfigurationManager.AppSettings["BruteForce:MaxConcurrentRequests"], out var value) ? value : 5;
+    
+    public static int MaxRetries => int.TryParse(ConfigurationManager.AppSettings["BruteForce:MaxRetries"], out var value) ? value : 200;
+    
+    // Настройки для CompanyListScraper
+    public static string CompaniesListUrl => ConfigurationManager.AppSettings["Companies:ListUrl"] ?? "https://career.habr.com/companies";
+    
+    public static string CompaniesBaseUrl => ConfigurationManager.AppSettings["Companies:BaseUrl"] ?? "https://career.habr.com/companies/";
+    
+    public static string CompaniesLinkSelector => ConfigurationManager.AppSettings["Companies:LinkSelector"] ?? "a[href^='/companies/']";
+    
+    public static string CompaniesHrefRegex => ConfigurationManager.AppSettings["Companies:HrefRegex"] ?? "/companies/([a-zA-Z0-9_-]+)";
+    
+    public static string CompaniesNextPageSelector => ConfigurationManager.AppSettings["Companies:NextPageSelector"] ?? "a.page[href*='page={0}']";
+    
+    // Настройки базы данных
+    public static string ConnectionString => ConfigurationManager.AppSettings["Database:ConnectionString"] ?? "Server=localhost:5432;User Id=postgres;Password=admin;Database=jobs;";
 }
